325c633ba8260f8800199a4b185152fb42149e2c
rkemp101 <rkemp101@gmail.com>
2025-07-04 11:07:55 +0200

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-  mapboxToken: 'pk.eyJ1IjoicmtlbXAxMDEiLCJhIjoiY2xwcXNmNDduMDRyajJpcDU0Nmtuc3VtMiJ9.fbU-Fe_DPHH8eu6rJQwvcg',
+  mapboxToken: 'MYKEY',
   mapStyle: 'mapbox://styles/rkemp101/clv9mgace00pu01ph56jx6b3t',
   apiUrl: 'http://localhost:8000/restaurants/'
 };
